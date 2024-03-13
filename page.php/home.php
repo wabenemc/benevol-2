b0cb3635ff1d9a875485229eec0645d1ede7722e
Wabene cedric <cedric.wabenemayele.simplon@gmail.com>
2024-03-13 19:25:35 +0100

    form

---
+++ page.php/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="section">
+        <label for="nom">Ton nom :</label>
+        <input type="text" id="nom" name="nom">
+    </div>
+    <div class="section">
+        <label for="age">Ton âge :</label>
+        <input type="number" id="age" name="age">
+    </div>
+    <div class="section">
+        <label for="animal">Ton animal préféré :</label>
+        <input type="text" id="animal" name="animal">
+    </div>
+    <div class="section">
+        <label for="message">Pourquoi il est spécial pour toi :</label>
+        <textarea id="message" name="message"></textarea>
+    </div>
+    <div class="section">
+        <button type="submit">Envoyer mon dessin !</button>
+    </div>
+</div>
+ 
+</body>
+</html>
